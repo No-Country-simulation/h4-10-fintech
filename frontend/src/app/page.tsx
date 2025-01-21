b99ceb0b12315d54065987911c7f7990e9dd3706
ledg555 <dev.ledg555@gmail.com>
2025-01-20 20:31:44 -0600

    feat: landing nav bar and footer

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Hero from "@/features/home-page/components/Hero";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen max-w-desktop mx-auto">
+      <Hero />
+    </main>
+  );
+}
--- frontend/src/features/home-page/components/Hero.tsx
+++ frontend/src/features/home-page/components/Hero.tsx
@@ -1,7 +1,21 @@
+// import Image from "next/image";
+// import heroImage from "@/assets/hero.jpg"
+
 export default function Hero() {
-  return(
-    <section>
-      
+  return (
+    <section className="text-center">
+      {/* <Image
+        src={heroImage}
+        width="1280"
+        height="40"
+        alt="Ahorro e inversiones"
+      /> */}
+      <div className="text-balance mt-[320px]">
+        <h1 className="font-bold text-6xl">iUpi</h1>
+        <p className="uppercase text-3xl tracking-widest py-4">
+          Ahorro & inversiones
+        </p>
+      </div>
     </section>
-  )
+  );
 }
