88e5028dc09da2e6ad2b8b5a5e3ea18ff1c99b7b
ledg555 <dev.ledg555@gmail.com>
2025-01-23 17:25:26 -0600

    fix: last commit

--- backend/iupi/src/main/java/com/iupi/fintech/controllers/UserController.java
+++
@@ -1,7 +0,0 @@
-package com.iupi.fintech.controllers;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class UserController {
-}
--- backend/iupi/src/main/java/com/iupi/fintech/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.iupi.fintech.repositories;
-
-import com.iupi.fintech.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByEmail(String email);
-}
