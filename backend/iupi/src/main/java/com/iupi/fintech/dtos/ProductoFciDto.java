a4e7d035d5658b2f97545e579d0a34e9052348b9
EdgarCamberos1894 <edgarcamberos18@gmail.com>
2025-02-06 01:34:24 -0700

    fix(correccion dto)

--- backend/iupi/src/main/java/com/iupi/fintech/dtos/ProductoFciDto.java
+++ backend/iupi/src/main/java/com/iupi/fintech/dtos/ProductoFciDto.java
@@ -7,30 +7,31 @@ import java.util.List;
 
 public record ProductoFciDto(
 
-//        Long id,
-//        String horizonteInversion,
-//        BigDecimal ultimoOperado,
-//        Double variacion,
-//        String tipoFondo,
-//        String invierte,
-//        String perfilInversor,
-//        Double variacionMensual,
-//        Double variacionAnual,
-//        String simbolo,
-//        String descripcion,
-//        String pais,
-//        String mercado,
-//        String tipo,
-//        BigDecimal montoMinimo,
-//        List<Long> transaccionesId
+        Long id,
+        String horizonteInversion,
+        BigDecimal ultimoOperado,
+        Double variacion,
+        String tipoFondo,
+        String invierte,
+        String perfilInversor,
+        Double variacionMensual,
+        Double variacionAnual,
+        String simbolo,
+        String descripcion,
+        String pais,
+        String mercado,
+        String tipo,
+        BigDecimal montoMinimo,
+        List<Long> transaccionesId
 
-        @JsonProperty("id_productosfci") int id,
-        @JsonProperty("prob") double probabilidad,
-        @JsonProperty("descripcion") String descripcion,
-        @JsonProperty("horizonte_inversion") String horizonteInversion,
-        @JsonProperty("instrumentos") String instrumentos,
-        @JsonProperty("perfil_inversor") String perfilInversor,
-        @JsonProperty("tipo_de_fondo") String tipoDeFondo
+        //Propiedades que retorna leo
+//        @JsonProperty("id_productosfci") int id,
+//        @JsonProperty("prob") double probabilidad,
+//        @JsonProperty("descripcion") String descripcion,
+//        @JsonProperty("horizonte_inversion") String horizonteInversion,
+//        @JsonProperty("instrumentos") String instrumentos,
+//        @JsonProperty("perfil_inversor") String perfilInversor,
+//        @JsonProperty("tipo_de_fondo") String tipoDeFondo
 
 
 ) {
